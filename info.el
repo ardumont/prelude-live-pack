366826277df88169d5d67ac5fbc854843744c535
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-05-20 09:08:44 +0200

    Rename the pack accordingly

--- info.el
+++ info.el
@@ -1,3 +1,3 @@
-(live-pack-name "prelude-pack")
+(live-pack-name "prelude-live-pack")
 (live-pack-version "0.0.2")
-(live-pack-description "prelude-pack - Wrapper around prelude-core which offers some awesome functions.")
+(live-pack-description "prelude-live-pack - Wrapper around prelude-core which offers some awesome functions.")
